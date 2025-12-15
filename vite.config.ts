538d79631b863799eb52d17ee344f4f7607113c7
lelongbao2790 <lelongbao2790@gmail.com>
2025-12-15 12:48:59 +0700

    Refactor Vite config for environment and server settings
    Refactor Vite configuration to load environment variables and set base URL based on mode. Added server configuration and path alias.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  const isProd = mode === 'production';
+
+  return {
+    base: isProd ? '/be-vui-hoc/' : '/'
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    }
+  };
+});
+
