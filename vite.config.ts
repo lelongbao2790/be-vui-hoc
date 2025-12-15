267d56adb81cad39db0018c4103b70c290f430d4
Brian Le <ble@ecapital.com>
2025-12-15 14:24:42 +0700

    Removed unneccessary file

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+	  base: mode === "production" ? "/be-vui-hoc/" : "/",
       plugins: [react(), tailwindcss()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
