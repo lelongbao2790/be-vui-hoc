9859540e1e77f023668ef010e16c3e5600ddfbe2
lelongbao2790 <lelongbao2790@gmail.com>
2025-12-15 12:55:46 +0700

    Fix formatting of base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
 
   return {
-    base: isProd ? '/be-vui-hoc/' : '/'
+    base: isProd ? '/be-vui-hoc/' : '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
